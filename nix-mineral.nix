231b885b652d51d32708e486d9ce2ef6b1ca06e4
cynicsketch <101476009+cynicsketch@users.noreply.github.com>
2024-07-30 10:56:10 -0400

    Justify use of jitterentropy

--- nix-mineral.nix
+++ nix-mineral.nix
@@ -904,6 +904,9 @@ imports = [ ./nm-overrides.nix ];
   };
 
   # Get extra entropy since we disabled hardware entropy sources
+  # Read more about why at the following URLs:
+  # https://github.com/smuellerDD/jitterentropy-rngd/issues/27
+  # https://blogs.oracle.com/linux/post/rngd1
   services.jitterentropy-rngd = { enable = true; };
   boot.kernelModules = [ ("jitterentropy_rng") ];
 
