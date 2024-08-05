0b22b09870fdb7b166262f952680b703e3d0a793
cynicsketch <101476009+cynicsketch@users.noreply.github.com>
2024-08-05 11:26:39 -0400

    Update bluetooth module blacklist

--- nm-overrides/security/disable-bluetooth.nix
+++ nm-overrides/security/disable-bluetooth.nix
@@ -13,8 +13,23 @@ with lib;
   config = mkIf config.nm-overrides.security.disable-bluetooth.enable {
     environment.etc."modprobe.d/nm-disable-bluetooth.conf" = {
       text = ''
-        install bluetooth /bin/disabled-bluetooth-by-security-misc
-        install btusb /bin/disabled-bluetooth-by-security-misc
+        install bluetooth /usr/bin/disabled-bluetooth-by-security-misc
+        install bluetooth_6lowpan  /usr/bin/disabled-bluetooth-by-security-misc
+        install bt3c_cs /usr/bin/disabled-bluetooth-by-security-misc
+        install btbcm /usr/bin/disabled-bluetooth-by-security-misc
+        install btintel /usr/bin/disabled-bluetooth-by-security-misc
+        install btmrvl /usr/bin/disabled-bluetooth-by-security-misc
+        install btmrvl_sdio /usr/bin/disabled-bluetooth-by-security-misc
+        install btmtk /usr/bin/disabled-bluetooth-by-security-misc
+        install btmtksdio /usr/bin/disabled-bluetooth-by-security-misc
+        install btmtkuart /usr/bin/disabled-bluetooth-by-security-misc
+        install btnxpuart /usr/bin/disabled-bluetooth-by-security-misc
+        install btqca /usr/bin/disabled-bluetooth-by-security-misc
+        install btrsi /usr/bin/disabled-bluetooth-by-security-misc
+        install btrtl /usr/bin/disabled-bluetooth-by-security-misc
+        install btsdio /usr/bin/disabled-bluetooth-by-security-misc
+        install btusb /usr/bin/disabled-bluetooth-by-security-misc
+        install virtio_bt /usr/bin/disabled-bluetooth-by-security-misc
       '';
     };
   };
