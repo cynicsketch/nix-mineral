1cc413d5820f6d182b427e5bead78fc2e0c6452b
cynicsketch <101476009+cynicsketch@users.noreply.github.com>
2025-05-16 15:50:03 -0400

    Add license notice

--- nm-overrides.nix
+++ nm-overrides.nix
@@ -1,3 +1,11 @@
+# This file is part of nix-mineral.
+#
+# nix-mineral is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+#
+# nix-mineral is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with Foobar. If not, see <https://www.gnu.org/licenses/>.
+
 # This is the overrides file for nix-mineral, containing a non-comprehensive
 # list of options that one may wish to override for any number of reasons.
 # 
