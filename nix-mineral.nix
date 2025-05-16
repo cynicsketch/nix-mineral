8cf94fd2936c6a70b3e333e4d6460761994ed10b
cynicsketch <101476009+cynicsketch@users.noreply.github.com>
2025-05-16 15:50:26 -0400

    Add license notice

--- nix-mineral.nix
+++ nix-mineral.nix
@@ -1,3 +1,11 @@
+# This file is part of nix-mineral.
+# 
+# nix-mineral is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+#
+# nix-mineral is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with Foobar. If not, see <https://www.gnu.org/licenses/>.
+
 # This is the main module for nix-mineral, containing the default configuration.
 
 # Primarily sourced was madaidan's Linux Hardening Guide. See for details: 
