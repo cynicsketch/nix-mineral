6f973873ad4145e05c26e680539feeb75d343d09
Seikm <git@seikm.com>
2025-07-27 21:52:50 -0300

    desktop.bluetooth-kmodules option

--- nix-mineral.nix
+++ nix-mineral.nix
@@ -328,6 +328,10 @@ in
                   and is similar behavior to handling USB as ChromeOS and GrapheneOS.
                 '' false;
               };
+
+              bluetooth-kmodules = mkBoolOption ''
+                Enable bluetooth related kernel modules.
+              '' true;
             };
           };
         };
@@ -479,6 +483,30 @@ in
                  subject.isInGroup("wheel")) { return polkit.Result.YES; }
           });
         '';
+      })
+
+      (l.mkIf (!cfg.settings.desktop.bluetooth-kmodules) {
+        environment.etc."modprobe.d/nm-disable-bluetooth.conf" = {
+          text = ''
+            install bluetooth /usr/bin/disabled-bluetooth-by-security-misc
+            install bluetooth_6lowpan  /usr/bin/disabled-bluetooth-by-security-misc
+            install bt3c_cs /usr/bin/disabled-bluetooth-by-security-misc
+            install btbcm /usr/bin/disabled-bluetooth-by-security-misc
+            install btintel /usr/bin/disabled-bluetooth-by-security-misc
+            install btmrvl /usr/bin/disabled-bluetooth-by-security-misc
+            install btmrvl_sdio /usr/bin/disabled-bluetooth-by-security-misc
+            install btmtk /usr/bin/disabled-bluetooth-by-security-misc
+            install btmtksdio /usr/bin/disabled-bluetooth-by-security-misc
+            install btmtkuart /usr/bin/disabled-bluetooth-by-security-misc
+            install btnxpuart /usr/bin/disabled-bluetooth-by-security-misc
+            install btqca /usr/bin/disabled-bluetooth-by-security-misc
+            install btrsi /usr/bin/disabled-bluetooth-by-security-misc
+            install btrtl /usr/bin/disabled-bluetooth-by-security-misc
+            install btsdio /usr/bin/disabled-bluetooth-by-security-misc
+            install btusb /usr/bin/disabled-bluetooth-by-security-misc
+            install virtio_bt /usr/bin/disabled-bluetooth-by-security-misc
+          '';
+        };
       })
 
       # Programs configurations
