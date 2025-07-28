7bbb17b62c8978fe1a6f3e57e0e48d4582bff1cb
Seikm <git@seikm.com>
2025-07-27 21:56:47 -0300

    kernel.intelme-kmodules option

--- nix-mineral.nix
+++ nix-mineral.nix
@@ -235,6 +235,22 @@ in
                 Set amd_iommu=force_isolation kernel parameter.
                 set this to false as workaround for hanging issue on linux kernel 6.13.
               '' true;
+
+              intelme-kmodules = mkBoolOption ''
+                Intel ME related kernel modules.
+                Disable this to avoid putting trust in the highly privilege ME system,
+                but there are potentially other consequences.
+
+                If you use an AMD system, you can disable this without negative consequence
+                and reduce attack surface.
+
+                Intel users should read more about the issue at the below links:
+                https://www.kernel.org/doc/html/latest/driver-api/mei/mei.html
+                https://en.wikipedia.org/wiki/Intel_Management_Engine#Security_vulnerabilities
+                https://www.kicksecure.com/wiki/Out-of-band_Management_Technology#Intel_ME_Disabling_Disadvantages
+                https://github.com/Kicksecure/security-misc/pull/236#issuecomment-2229092813
+                https://github.com/Kicksecure/security-misc/issues/239
+              '' true;
             };
           };
         };
@@ -420,6 +436,25 @@ in
         boot.kernelParams = [
           "amd_iommu=force_isolation"
         ];
+      })
+
+      (l.mkIf (!cfg.settings.kernel.intelme-kmodules) {
+        environment.etc."modprobe.d/nm-disable-intelme-kmodules.conf" = {
+          text = ''
+            install mei /usr/bin/disabled-intelme-by-security-misc
+            install mei-gsc /usr/bin/disabled-intelme-by-security-misc
+            install mei_gsc_proxy /usr/bin/disabled-intelme-by-security-misc
+            install mei_hdcp /usr/bin/disabled-intelme-by-security-misc
+            install mei-me /usr/bin/disabled-intelme-by-security-misc
+            install mei_phy /usr/bin/disabled-intelme-by-security-misc
+            install mei_pxp /usr/bin/disabled-intelme-by-security-misc
+            install mei-txe /usr/bin/disabled-intelme-by-security-misc
+            install mei-vsc /usr/bin/disabled-intelme-by-security-misc
+            install mei-vsc-hw /usr/bin/disabled-intelme-by-security-misc
+            install mei_wdt /usr/bin/disabled-intelme-by-security-misc
+            install microread_mei /usr/bin/disabled-intelme-by-security-misc
+          '';
+        };
       })
 
       # System configurations
