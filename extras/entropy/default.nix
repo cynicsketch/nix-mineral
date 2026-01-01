f1da1e88ba7672d91cf4f3dea4763bc0ea507269
cynicsketch <cynicsketch.wasp956@silomails.com>
2026-01-01 10:22:33 -0500

    Initialize extra entropy settings

---
+++ extras/entropy/default.nix
@@ -0,0 +1,54 @@
+# This file is part of nix-mineral (https://github.com/cynicsketch/nix-mineral/).
+# Copyright (c) 2025 cynicsketch
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+{
+  options,
+  config,
+  pkgs,
+  lib,
+  l,
+  cfg,
+  ...
+}:
+
+let
+  categoryModules =
+    l.mkCategoryModules cfg
+      [
+        ./extra-latent-entropy.nix
+      ]
+      {
+        inherit
+          options
+          config
+          pkgs
+          lib
+          ;
+      };
+in
+{
+  options = {
+    kernel = l.mkOption {
+      description = ''
+        Modify entropy settings for improved security
+      '';
+      default = { };
+      type = l.mkCategorySubmodule categoryModules;
+    };
+  };
+
+  config = l.mkCategoryConfig categoryModules;
+}
