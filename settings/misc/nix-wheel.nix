218559eba64b7a0d0c73878f9e255f2b5521673f
Angel J <78835633+iamanaws@users.noreply.github.com>
2026-01-03 12:33:16 -0800

    refactor: remove redundant nix-allow-only-wheel option

--- settings/misc/nix-wheel.nix
+++ settings/misc/nix-wheel.nix
@@ -23,7 +23,8 @@
 {
   options = {
     nix-wheel = l.mkBoolOption ''
-      Limit access to nix to users with the "wheel" group. ("sudoers")
+      Limit access to nix commands to users with the "wheel" group. ("sudoers")
+      if false, may be useful for allowing a non-wheel user to, for example, use devshell.
     '' true;
   };
 
