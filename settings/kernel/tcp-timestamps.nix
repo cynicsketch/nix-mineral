11066008dcc20be9aae22b2924f206f8af55f13b
cynicsketch <cynicsketch.wasp956@silomails.com>
2026-01-13 18:10:04 -0500

    Update tcp timestamps documentation

--- settings/kernel/tcp-timestamps.nix
+++ settings/kernel/tcp-timestamps.nix
@@ -27,9 +27,13 @@
       Disabling prevents leaking system time, enabling protects against
       wrapped sequence numbers and improves performance.
 
-      Read more about the issue here:
-      URL: (In favor of disabling): https://madaidans-insecurities.github.io/guides/linux-hardening.html#tcp-timestamps
-      URL: (In favor of enabling): https://access.redhat.com/sites/default/files/attachments/20150325_network_performance_tuning.pdf
+      In favor of disabling:
+      https://madaidans-insecurities.github.io/guides/linux-hardening.html#tcp-timestamps
+      https://forums.whonix.org/t/do-ntp-and-tcp-timestamps-really-leak-your-local-time/7824/6
+
+      In favor of enabling:
+      https://access.redhat.com/sites/default/files/attachments/20150325_network_performance_tuning.pdf
+      https://gitlab.tails.boum.org/tails/tails/-/issues/17491
     '' true;
   };
 
