a925319978a967949c849d4de107ca9db62867b6
Seikm <git@seikm.com>
2026-01-15 13:30:05 -0300

    Generate presets configs automatically

--- presets/default.nix
+++ presets/default.nix
@@ -23,6 +23,14 @@
 
 let
   cfg = config.nix-mineral;
+
+  presets = {
+    maximum = "enables every optional security setting to have maximum protection";
+    compatibility = "disables or enables settings to aim at compatibility";
+    performance = "disables or enables settings to aim at performance";
+  };
+
+  presetsEnum = l.types.enum ([ "default" ] ++ (l.attrNames presets));
 in
 {
   options = {
@@ -32,18 +40,13 @@ in
           The preset to use for the nix-mineral module.
           (all presets are applied on top of the default preset)
 
-          - maximum: enables every optional security setting to have maximum protection.
           - default: only default settings.
-          - compatibility: disables or enables settings to aim at compatibility.
-          - performance: disables or enables settings to aim at performance.
+          ${l.concatStringsSep "\n" (
+            l.mapAttrsToList (name: description: "- ${name}: ${description}.") presets
+          )}
         '';
         default = "default";
-        type = l.types.enum [
-          "maximum"
-          "default"
-          "compatibility"
-          "performance"
-        ];
+        type = presetsEnum;
       };
     };
   };
@@ -65,13 +68,10 @@ in
     in
     # Any other way of doing this causes infinite recursion for some reason
     # i tried using a hashtable, if elses, but the only one that worked was mkIf
-    l.mkMerge [
-      (l.mkIf (cfg.preset == "maximum") (importWithArgs ./maximum.nix))
-
-      (l.mkIf (cfg.preset == "default") { })
-
-      (l.mkIf (cfg.preset == "compatibility") (importWithArgs ./compatibility.nix))
-
-      (l.mkIf (cfg.preset == "performance") (importWithArgs ./performance.nix))
-    ];
+    l.mkMerge (
+      l.mapAttrsToList (
+        name: _:
+        if name == "default" then { } else (l.mkIf (cfg.preset == name) (importWithArgs ./${name}.nix))
+      ) presets
+    );
 }
