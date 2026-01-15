f8779bbfac63704cd050f9f8fd04ac2c372fd655
Seikm <git@seikm.com>
2026-01-15 14:16:01 -0300

    Change all presets to use mkPresets

--- presets/maximum.nix
+++ presets/maximum.nix
@@ -15,25 +15,25 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 {
-  mkPreset,
+  mkPresets,
   ...
 }:
 
 {
-  nix-mineral = {
+  nix-mineral = mkPresets {
     settings = {
       kernel = {
         # Enable SAK (Secure Attention Key). SAK prevents keylogging, if used correctly.
-        sysrq = mkPreset "sak";
+        sysrq = "sak";
       };
 
       system = {
         # Restrict yama ptrace scope to the most secure option.
         # No processes may be traced with ptrace.
-        yama = mkPreset "restricted";
+        yama = "restricted";
 
         # Never allow processes to modify their own memory mappings.
-        proc-mem-force = mkPreset "never";
+        proc-mem-force = "never";
       };
     };
 
@@ -46,44 +46,44 @@
         # https://www.kicksecure.com/wiki/Out-of-band_Management_Technology#Intel_ME_Disabling_Disadvantages
         # https://github.com/Kicksecure/security-misc/pull/236#issuecomment-2229092813
         # https://github.com/Kicksecure/security-misc/issues/239
-        intelme-kmodules = mkPreset false;
+        intelme-kmodules = false;
       };
 
       system = {
         # Lock the root account. Requires another method of privilege escalation, i.e
         # sudo or doas, and declarative accounts to work properly.
-        lock-root = mkPreset true;
+        lock-root = true;
 
         # Reduce swappiness to bare minimum. May reduce risk of writing sensitive
         # information to disk, but hampers zram performance. Also useless if you do
         # not even use a swap file/partition, i.e zram only setup.
-        minimize-swapping = mkPreset true;
+        minimize-swapping = true;
 
         # Replace systemd-timesyncd with chrony for NTP, and configure chrony for NTS
         # and to use the seccomp filter for security.
-        secure-chrony = mkPreset true;
+        secure-chrony = true;
 
         # if false, this may break some applications that rely on user namespaces.
-        unprivileged-userns = mkPreset false;
+        unprivileged-userns = false;
       };
 
       network = {
         # Disable bluetooth related kernel modules. (breaks bluetooth)
-        bluetooth-kmodules = mkPreset false;
+        bluetooth-kmodules = false;
 
         # if false, may help mitigate TCP reset DoS attacks, but
         # may also harm network performance when at high latencies.
-        tcp-window-scaling = mkPreset false;
+        tcp-window-scaling = false;
       };
 
       misc = {
         # Replace sudo with doas, doas has a lower attack surface, but is less audited.
-        replace-sudo-with-doas = mkPreset true;
-        doas-sudo-wrapper = mkPreset true;
+        replace-sudo-with-doas = true;
+        doas-sudo-wrapper = true;
 
         # Enable USBGuard, a tool to restrict USB devices.
         # (blocks any USB devices, maybe enable usbguard.whitelist-at-boot)
-        usbguard.enable = mkPreset true;
+        usbguard.enable = true;
       };
     };
   };
--- presets/performance.nix
+++ presets/performance.nix
@@ -15,41 +15,41 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 {
-  mkPreset,
+  mkPresets,
   ...
 }:
 
 {
-  nix-mineral = {
+  nix-mineral = mkPresets {
     settings = {
       kernel = {
         # if false, may prevent low resource systems from booting.
-        busmaster-bit = mkPreset true;
+        busmaster-bit = true;
 
         # Enable symmetric multithreading and just use default CPU mitigations,
         # to potentially improve performance.
         # DO NOT disable all cpu mitigations,
-        cpu-mitigations = mkPreset "smt-on";
+        cpu-mitigations = "smt-on";
 
         # Could increase I/O performance on ARM64 systems, with risk.
-        iommu-passthrough = mkPreset true;
+        iommu-passthrough = true;
 
         # PTI (Page Table Isolation) may tax performance.
-        pti = mkPreset false;
+        pti = false;
 
         # Don't use kcfi as the control flow implementation in the kernel,
         # since it performs worse than FineIBT, which is the current Linux
         # kenrel (not nix-mineral) default.
-        kcfi = mkPreset false;
+        kcfi = false;
 
         # Do not enable red zoning and sanity checking with slab debug, since
         # it adds significant memory allocation overhead.
-        slab-debug = mkPreset false;
+        slab-debug = false;
       };
 
       system = {
         # allow 32-bit libraries and applications to run.
-        multilib = mkPreset true;
+        multilib = true;
       };
     };
   };
